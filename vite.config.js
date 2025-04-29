58831a2368f201ef1ed1a84067eb7cd1bdde51eb
guru <gurujii555z5@gmail.com>
2025-04-29 15:47:09 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,19 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  // depending on your application, base can also be "/"
   const env = loadEnv(mode, process.cwd(), '');
   const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = 3000;
 
   return {
+    base: '/', // ✔ Use '/' for Vercel deployment (not './')
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
       host: true
     },
     build: {
+      outDir: 'dist', // ✔ Make sure build goes to /dist (Vercel default)
       chunkSizeWarningLimit: 1600
     },
     preview: {
@@ -28,23 +27,9 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        // { find: '', replacement: path.resolve(__dirname, 'src') },
-        // {
-        //   find: /^~(.+)/,
-        //   replacement: path.join(process.cwd(), 'node_modules/$1')
-        // },
-        // {
-        //   find: /^src(.+)/,
-        //   replacement: path.join(process.cwd(), 'src/$1')
-        // }
-        // {
-        //   find: 'assets',
-        //   replacement: path.join(process.cwd(), 'src/assets')
-        // },
         '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
       }
     },
-    base: '/',
     plugins: [react(), jsconfigPaths()]
   };
 });
