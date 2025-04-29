872ebd7d001873fa25dae48cb1da934b7a979b67
guru <gurujii555z5@gmail.com>
2025-04-29 16:31:14 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  // depending on your application, base can also be "/"
   const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = 3000;
+
+  const isDev = mode === 'development';
 
   return {
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
       host: true
     },
@@ -28,23 +26,10 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        // { find: '', replacement: path.resolve(__dirname, 'src') },
-        // {
-        //   find: /^~(.+)/,
-        //   replacement: path.join(process.cwd(), 'node_modules/$1')
-        // },
-        // {
-        //   find: /^src(.+)/,
-        //   replacement: path.join(process.cwd(), 'src/$1')
-        // }
-        // {
-        //   find: 'assets',
-        //   replacement: path.join(process.cwd(), 'src/assets')
-        // },
         '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
       }
     },
-    base: '/free',
+    base: isDev ? '/free' : '/', // 👈 Important fix here
     plugins: [react(), jsconfigPaths()]
   };
 });
